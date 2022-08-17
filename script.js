42f05e5efa234ef6a21a7ab18d3db2ea8fa373f3
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-17 18:57:20 -0400

    fixed wordCross funct to work with newly added li

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-
-function inputLength() {
-	return input.value.length;
-}
-
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
-
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
 // var button = document.getElementById("enter");
 // var input = document.getElementById("userinput");
 // var ul = document.querySelector("ul");
-// var listItems = document.getElementsByClassName("list");
+
 
 // function inputLength() {
 // 	return input.value.length;
@@ -42,12 +11,6 @@ input.addEventListener("keypress", addListAfterKeypress);
 // 	var li = document.createElement("li");
 // 	li.appendChild(document.createTextNode(input.value));
 // 	ul.appendChild(li);
-// 	li.classList.add("list");
-
-// 	var listBtn = document.createElement("button");
-// 	listBtn.textContent = "Delete";
-// 	listBtn.classList.add("delBtn");
-// 	li.appendChild(listBtn);
 // 	input.value = "";
 // }
 
@@ -63,16 +26,62 @@ input.addEventListener("keypress", addListAfterKeypress);
 // 	}
 // }
 
-// // function wordCross() {
-// // 	listItem.classlist.toggle("done");
-// // }
-
 // button.addEventListener("click", addListAfterClick);
 
 // input.addEventListener("keypress", addListAfterKeypress);
 
-// Array.from(listItems).forEach(listItem => {
-// 	listItem.addEventListener("click", () => {
-// 		listItem.classList.toggle("done");
-// 		});
-// 	});
+
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var listItems = document.getElementsByClassName("list");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.classList.add("list");
+	ul.appendChild(li);
+	var listBtn = document.createElement("button");
+	listBtn.textContent = "Delete";
+	listBtn.classList.add("delBtn");
+	li.appendChild(listBtn);
+	input.value = "";
+
+	wordCross();
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+// function wordCross() {
+// 	listItem.classlist.toggle("done");
+// }
+
+function wordCross() {
+	Array.from(listItems).forEach(listItem => {
+		listItem.addEventListener("click", () => {
+			listItem.classList.toggle("done");
+			console.log(listItem)
+			});
+		});
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+wordCross();
