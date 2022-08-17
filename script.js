f6aad531cee4ccc6263de3a9052c876a7a5895f1
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-17 16:08:02 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var listItems = document.getElementsByClassName("list");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	li.classList.add("list");
+
+	var listBtn = document.createElement("button");
+	listBtn.textContent = "Delete";
+	listBtn.classList.add("delBtn");
+	li.appendChild(listBtn);
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+// function wordCross() {
+// 	listItem.classlist.toggle("done");
+// }
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+Array.from(listItems).forEach(listItem => {
+	listItem.addEventListener("click", () => {
+		listItem.classList.toggle("done");
+		});
+	});
