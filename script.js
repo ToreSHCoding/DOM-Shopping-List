1317cb111c3d9f373b72ec8eb714a7d59856c285
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-17 16:12:24 -0400

    initial file with edit commented out on the bottom

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var listItems = document.getElementsByClassName("list");
 
 function inputLength() {
 	return input.value.length;
@@ -11,12 +10,6 @@ function createListElement() {
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
-	li.classList.add("list");
-
-	var listBtn = document.createElement("button");
-	listBtn.textContent = "Delete";
-	listBtn.classList.add("delBtn");
-	li.appendChild(listBtn);
 	input.value = "";
 }
 
@@ -32,16 +25,54 @@ function addListAfterKeypress(event) {
 	}
 }
 
-// function wordCross() {
-// 	listItem.classlist.toggle("done");
-// }
-
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-Array.from(listItems).forEach(listItem => {
-	listItem.addEventListener("click", () => {
-		listItem.classList.toggle("done");
-		});
-	});
+// var button = document.getElementById("enter");
+// var input = document.getElementById("userinput");
+// var ul = document.querySelector("ul");
+// var listItems = document.getElementsByClassName("list");
+
+// function inputLength() {
+// 	return input.value.length;
+// }
+
+// function createListElement() {
+// 	var li = document.createElement("li");
+// 	li.appendChild(document.createTextNode(input.value));
+// 	ul.appendChild(li);
+// 	li.classList.add("list");
+
+// 	var listBtn = document.createElement("button");
+// 	listBtn.textContent = "Delete";
+// 	listBtn.classList.add("delBtn");
+// 	li.appendChild(listBtn);
+// 	input.value = "";
+// }
+
+// function addListAfterClick() {
+// 	if (inputLength() > 0) {
+// 		createListElement();
+// 	}
+// }
+
+// function addListAfterKeypress(event) {
+// 	if (inputLength() > 0 && event.keyCode === 13) {
+// 		createListElement();
+// 	}
+// }
+
+// // function wordCross() {
+// // 	listItem.classlist.toggle("done");
+// // }
+
+// button.addEventListener("click", addListAfterClick);
+
+// input.addEventListener("keypress", addListAfterKeypress);
+
+// Array.from(listItems).forEach(listItem => {
+// 	listItem.addEventListener("click", () => {
+// 		listItem.classList.toggle("done");
+// 		});
+// 	});
