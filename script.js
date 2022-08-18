42d58d393d77215581129a61268fd3abb935cbc8
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-18 09:16:23 -0400

    added working delete function to delete list items

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var listItems = document.getElementsByClassName("list");
+var delBtn = document.getElementsByClassName("delBtn");
 
 function inputLength() {
 	return input.value.length;
@@ -50,6 +51,8 @@ function createListElement() {
 	ul.appendChild(li);
 	var listBtn = document.createElement("button");
 	listBtn.textContent = "Delete";
+	// allows delListItem() to work with newly created li/delBtn elements from DOM
+	listBtn.addEventListener("click", delListItem);
 	listBtn.classList.add("delBtn");
 	li.appendChild(listBtn);
 	input.value = "";
@@ -80,12 +83,7 @@ function wordCrossToggle() {
 // 		});
 // }
 
-// for loop that allows wordCrossToggle() on existing li elements within HTML
-for (var i = 0; i < listItems.length; i++) {
-	listItems[i].addEventListener("click", wordCrossToggle);
-}
-
-// ======ALTERNATIVE WAY TO MAKE WORDCROSS TOGGLE WORK=======
+// ======ALTERNATIVE WAY TO MAKE WORDCROSS TOGGLE WORK THROUGH EVENT DELEGATION=======
 
 // var ulTag = document.getElementById("ul-tag");
 
@@ -98,8 +96,23 @@ for (var i = 0; i < listItems.length; i++) {
 // 	console.log(target);
 // })
 
-// ===========================================================
+// ====================================================================================
+
+// function to allow list items to be deleted when delBtn is pressed
+function delListItem() {
+	this.parentNode.remove()
+}
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+// for loop that allows wordCrossToggle() on existing li elements within HTML
+for (var i = 0; i < listItems.length; i++) {
+	listItems[i].addEventListener("click", wordCrossToggle);
+}
+
+// for loop that allows delListItem() on existing delBtn elements within HTML
+for (var i = 0; i < delBtn.length; i++) {
+	delBtn[i].addEventListener("click", delListItem)
+}
