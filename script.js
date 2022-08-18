a7dc6b7ee5589f77cbfcd5bed2eb6302e6438193
Sayed Haamid Tore <toreshcoding@gmail.com>
2022-08-18 12:27:25 -0400

    completed CSS except for media queries

--- script.js
+++ script.js
@@ -56,6 +56,9 @@ function createListElement() {
 	listBtn.addEventListener("click", delListItem);
 	listBtn.classList.add("delBtn");
 	li.appendChild(listBtn);
+	var lineBreak = document.createElement("hr");
+	lineBreak.classList.add("line-break");
+	li.appendChild(lineBreak);
 	input.value = "";
 }
 
